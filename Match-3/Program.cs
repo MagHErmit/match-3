d552bac0296b93808c631b897f5914bf196fe868
Владислав Какурин <kakurin.vs@students.dvfu.ru>
2020-07-22 15:39:17 +1000

    Install SFML .Net and JSON converter; Add "Hello world" functional

---
+++ Match-3/Program.cs
@@ -0,0 +1,22 @@
+﻿using SFML.Graphics;
+using SFML.Window;
+using System;
+
+namespace Match_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RenderWindow window =
+                 new RenderWindow(new VideoMode(800, 600), "Hello SFML!");
+
+            while (window.IsOpen == true)
+            {
+                window.DispatchEvents();
+
+                window.Display();
+            }
+        }
+    }
+}
