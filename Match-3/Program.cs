37f171cbf0c06d9fe87eb6fcbcc23661613c119c
Vladislav Kakurin <vkakurin@game-forest.com>
2020-07-29 14:20:21 +1000

    Add background

--- Match-3/Program.cs
+++ Match-3/Program.cs
@@ -6,15 +6,21 @@ namespace Match_3
 {
     class Program
     {
+        public static string path = "..\\..\\..\\";
         static void Main(string[] args)
         {
             RenderWindow window =
-                 new RenderWindow(new VideoMode(800, 600), "Hello SFML!");
-
-            while (window.IsOpen == true)
+                 new RenderWindow(new VideoMode(740, 480), "Match 3");
+            window.SetVerticalSyncEnabled(true);
+            window.SetFramerateLimit(60);
+            Texture tb = new Texture(path + "Textures\\background.png");
+            Sprite background = new Sprite(tb);
+            window.Draw(background);
+            window.Closed += (_, e) => window.Close();
+            while (window.IsOpen)
             {
                 window.DispatchEvents();
-
+                window.Clear(Color.Black);
                 window.Display();
             }
         }
